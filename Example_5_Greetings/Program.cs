44b2a0e28852a1447742128dcf26bf070d50acac
Sergey Z <zassipkin.com>
2022-08-04 16:20:06 +0300

    Example_5_Greetings

---
+++ Example_5_Greetings/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Type your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Masha!!! My Darling!!! How are you doing?");    
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.Write(username);
+    Console.WriteLine("!");
+}
