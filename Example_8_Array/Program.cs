6272d963441765a4d9c27c307a99c7f8ae0d7802
Sergey Z <zassipkin.com>
2022-08-08 12:17:15 +0300

    Example 8

---
+++ Example_8_Array/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 7;
+int b1 = 10;
+int c1 = 3;
+int a2 = 18;
+int b2 = 28;
+int c2 = 99981;
+int a3 = 100;
+int b3 = 19;
+int c3 = 31;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
