878b68bd7f0e13e4771c95cbb5f1364ca33a6f97
Sergey Z <zassipkin.com>
2022-08-08 12:26:15 +0300

    Example 9

---
+++ Example_9_Array_intro/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {13, 73, 84, 10, 7, 435, 478, 65, 100};
+
+array[0] = 20;
+
+Console.WriteLine(array[3]);
